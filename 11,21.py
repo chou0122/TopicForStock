66922e7750638414850297eea6ba7590c8117ae5
chou0122 <dannychou0122@gmail.com>
2023-11-21 20:49:07 +0800

    Update 11,21.py

--- 11,21.py
+++ 11,21.py
@@ -1,9 +1,9 @@
 import shioaji as sj  #引入shioaji套件，命名為sj
 api = sj.Shioaji()  #建立shioaji api物件
 api.login(   #登入
-    u_id="",  #帳號
-    passwd="",  #密碼
+    person_id="1",  #帳號
+    passwd="1",  #密碼
     contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done.")
 )
-contract_2890 = api.Contracts.Stocks["2230"]
-print(contract_2230)
+contract_2890 = api.Contracts.Stocks["2330"]
+print(contract_2330)

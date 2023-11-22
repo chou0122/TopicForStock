def977a20bb176a17e4b37142e5647b226fb907d
chou0122 <dannychou0122@gmail.com>
2023-11-22 18:19:47 +0800

    更改python版本

--- 11,21.py
+++ 11,21.py
@@ -1,8 +1,8 @@
 import shioaji as sj  #引入shioaji套件，命名為sj
 api = sj.Shioaji()  #建立shioaji api物件
 api.login(   #登入
-    person_id="1",  #帳號
-    passwd="1",  #密碼
+    person_id="F129984358",  #帳號
+    passwd="DAnnychou0122@",  #密碼
     contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done.")
 )
 contract_2890 = api.Contracts.Stocks["2330"]

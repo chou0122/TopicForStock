121b297df0ad18337f995154a3c72e5ba2992e4a
chou0122 <dannychou0122@gmail.com>
2023-12-26 19:42:55 +0800

    ...

--- 11,22.py
+++ 11,22.py
@@ -1,9 +1,9 @@
 import shioaji as sj  #引入shioaji套件，命名為sj
 api = sj.Shioaji()  #建立shioaji api物件
 api.login(   #登入
-    person_id="F129984358",  #帳號
-    passwd="DAnnychou0122@",  #密碼
+    person_id="5Msqh9X4D6e7ztJChjac4Py43APt4fUKAizsY5HYvxCk",  #帳號
+    passwd="982UoLdhHNLD56Q4H95geXFLopBd4EoXNBrS87eFo93G",  #密碼
     contracts_cb=lambda security_type: print(f"{repr(security_type)} fetch done.")
 )
-contract_2890 = api.Contracts.Stocks["2330"]
-print(contract_2330)
+contract_2890 = api.Contracts.Stocks["2890"]
+print(contract_2890)
